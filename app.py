adc573e77ea9b019b2f9eb36c87512a34366f3d8
superiorsd10 <sachindapkara6@gmail.com>
2023-05-20 11:02:44 +0530

    completing the api

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify, abort
 
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+path = '/Users/sachin/Desktop/Python Academics/College GateKeeper/college-gatekeeper-flask/college-gatekeeper-firebase-adminsdk-3ss20-fdf904afec.json'
+
+cred = credentials.Certificate(path)
+firebase_admin.initialize_app(cred)
+
+
 # for base 64 encoding and decoding purposes
 import base64
 
@@ -72,6 +81,13 @@ def upload_image():
         # if matched, assigning the roll number
         if match:
             roll_number = match.group()
+            roll_number_branch = roll_number[0:3]
+            is_found_dollar = roll_number_branch.find("$")
+            if is_found_dollar:
+                roll_number.replace("$", "S")
+            is_found_one = roll_number_branch.find("1")
+            if is_found_one:
+                roll_number.replace("1", "I")
             print(roll_number)  # Output: LCS2021005
         else:
             print("Roll number not found.")
@@ -79,6 +95,57 @@ def upload_image():
         
         # senting the roll number to client side in the form of json object
         return jsonify({'message': roll_number})
+    except KeyError:
+        abort(400, 'Invalid request body')
+    except:
+        abort(500, 'Internal server error')
+
+
+@app.route('/confirm_roll_number', methods=['POST'])
+def confirm_roll_number():
+    try:
+        # getting the json data from the client side after confirming the roll number
+        json_data = request.get_json()
+
+        # accessing the confirm roll_number field
+        is_roll_number_confirm = json_data['confirm']
+
+        # accessing the roll number 
+        roll_number = json_data['roll_number']
+
+        # recording the current date
+        date = datetime.datetime.now().strftime("%d-%m-%y")
+
+        # if the sent roll number was correct
+        if is_roll_number_confirm:
+            db = firestore.client()
+
+            # referring to the data collection with date document 
+            collection_ref = db.collection('data').document(date).collection('entries')
+
+            # referring to the document with confirmed roll number
+            search_roll_number_ref = collection_ref.document(roll_number)
+
+            # getting the snapshot of the referred document
+            search_roll_number_snapshot = search_roll_number_ref.get()
+
+            # if the document already exists
+            if search_roll_number_snapshot.exists:
+
+                # deleting the document and returning the json response
+                search_roll_number_ref.delete()
+                return jsonify({'message': 'in-time recorded'})
+            else:
+
+                # else recording the data and inserting the document
+                data = {
+                    'Roll No': roll_number, 
+                    'In Time': firestore.SERVER_TIMESTAMP, 
+                    'Server Timestamp': firestore.SERVER_TIMESTAMP,
+                }
+                doc_ref = collection_ref.document(roll_number)
+                doc_ref.set(data)
+                return jsonify({'message': 'out-time recorded'})
     except KeyError:
         abort(400, 'Invalid request body')
     except:
